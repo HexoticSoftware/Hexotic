3ed1a43c798796f62f394fa1e4942ffd6ae852e3
md74671 <mcditchman@gmail.com>
2016-04-07 13:25:40 -0400

    Messing with home page. Ported over current home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,67 @@
+<html lang="en">
+<head>
+    <title>Hexotic Software</title>
+    <link href='http://fonts.googleapis.com/css?family=Orbitron:400,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('styles/main.css') }}"/>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+</head>
+<body>
+<br/><br/><br/>
+<h1>Hexotic Software</h1>
+<h3>A New Kind of Software</h3>
+
+<h5>Currently Under Construction</h5>
+<br/>
+<h5>Other Projects in Development:</h5>
+<div class="softwareContainer">
+    <img src="http://www.daemonnation.com/images/logos/LargeIcon.png" width="100px" height="100px"/>
+    <br/><br/>
+    Social Networking<br/><br/>
+    <a href="http://www.daemonnation.com">Check it out</a>
+</div>
+<div class="softwareContainer"><img src="http://hexotic.net/software/utils/images/icons/default.png" width="100px" height="100px"/>
+    <br/><br/>
+    List of all projects<br/><br/>
+    <a href="http://hexotic.net/software/utils/">Check it out</a>
+</div>
+<div class="softwareContainer"><img src="http://hexotic.net/software/ytdownloader/images/imotbo.png" width="100px" height="100px"/>
+    <br/><br/>
+    Sandbox Site<br/><br/>
+    <a href="http://www.imotbo.com">Check it out</a>
+</div>
+<div class="softwareContainer"><img src="http://hexotic.net/software/ytdownloader/images/logo.png" width="100px" height="100px"/>
+    <br/><br/>
+    YouTube Downloader<br/><br/>
+    <a href="http://hexotic.net/software/ytdownloader/">Check it out</a>
+</div>
+<br/>
+<div class="softwareContainer"><img src="http://hexotic.net/software/scryptr/images/ScryptrMain.png" width="100px" height="100px"/>
+    <br/><br/>
+    Scryptr<br/><br/>
+    <a href="http://hexotic.net/software/scryptr/">Check it out</a>
+</div>
+
+<div class="softwareContainer"><img src="http://hexotic.net/software/utils/images/icons/default.png" width="100px" height="100px"/>
+    <br/><br/>
+    T3<br/><br/>
+    coming soon
+</div>
+<div class="softwareContainer"><img src="http://hexotic.net/software/utils/images/icons/default.png" width="100px" height="100px"/>
+    <br/><br/>
+    Awesome Image Viewer 2.0<br/><br/>
+    coming soon
+</div>
+
+<div class="softwareContainer"><img src="http://donate.hexotic.net/beer.png" width="100px" height="100px"/>
+    <br/><br/>
+    Buy us a beer<br/><br/>
+    <a  href="http://donate.hexotic.net/">Donate Page</a>
+</div>
+
+
+<br/><br/><br/><br/>
+<div style="font-size:10px;font-family:arial;color:#ababab;">
+    Copyright Hexotic Software 2015.  All Rights Reserved.
+</div>
+</body>
+</html>
